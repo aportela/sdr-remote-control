0b86fff2d45990d82559c4a7b1b42791d1f1fe70
alex <766f6964+github@gmail.com>
2024-06-08 00:33:19 +0200

    added animation interface & abstract classes

---
+++ SSWAnimation.h
@@ -0,0 +1,40 @@
+#ifndef SDR_REMOTE_CONTROL_SPECTRUM_SCOPE_WATERFALL_ANIMATION_H
+#define SDR_REMOTE_CONTROL_SPECTRUM_SCOPE_WATERFALL_ANIMATION_H
+
+#include <Arduino.h>
+#include "ISSWAnimation.h"
+
+#define SSWA_WIDTH 160
+#define SSWA_SPECTRUM_SCOPE_WIDTH SSWA_WIDTH
+#define SSWA_SPECTRUM_SCOPE_HEIGHT 24
+#define SSWA_WATERFALL_WIDTH SSWA_WIDTH
+#define SSWA_WATERFALL_HEIGHT 64
+
+#define SSWA_MIN_DIST_BETWEEN_CW_SIGNALS 8
+#define SSWA_MAX_DIST_BETWEEN_CW_SIGNALS 12
+#define SSWA_TOTAL_CW_SIGNALS_BANDWITH 60  // (first 60 = morse, remaining = analog voice)
+
+#define SSWA_MIN_DIST_BETWEEN_ANALOG_VOICE_SIGNALS 16
+#define SSWA_MAX_DIST_BETWEEN_ANALOG_VOICE_SIGNALS 32
+
+#define SSWA_MIN_NOISE_VALUE 0
+#define SSWA_MAX_NOISE_VALUE 48
+
+#define SSWA_MIN_SIGNAL_VALUE 80
+#define SSWA_MAX_SIGNAL_VALUE 255
+
+class SSWAnimation : public ISSWAnimation {
+public:
+  virtual void refresh(uint16_t xOffset, uint16_t yOffset) override = 0;
+protected:
+  uint16_t signalsData[SSWA_WIDTH];
+  uint16_t noiseData[SSWA_WIDTH];
+  uint16_t signalIndexes[SSWA_WIDTH];
+  // generate random noise data
+  void refreshNoise(void);
+  // generate start/default signals levels
+  void initSignals(void);
+  // regenerate signals levels
+  void refreshSignals(void);
+};
+#endif
--- SSWAnimationST7789.h
+++ SSWAnimationST7789.h
@@ -1,30 +1,20 @@
-#ifndef SDR_REMOTE_CONTROL_SPECTRUM_SCOPE_WATERFALL_ANIMATION_H
-#define SDR_REMOTE_CONTROL_SPECTRUM_SCOPE_WATERFALL_ANIMATION_H
+#ifndef SDR_REMOTE_CONTROL_SPECTRUM_SCOPE_WATERFALL_ANIMATION_ST7789_H
+#define SDR_REMOTE_CONTROL_SPECTRUM_SCOPE_WATERFALL_ANIMATION_ST7789_H
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 
-#define SSWA_WIDTH 160
+#include "SSWAnimation.h"
 
-class SSWAnimationST7789 {
+class SSWAnimationST7789 : public SSWAnimation {
 public:
   SSWAnimationST7789(Adafruit_ST7789* existingDisplay);
   ~SSWAnimationST7789();
-  // refresh animation
-  void refresh(uint16_t xOffset, uint16_t yOffset);
+  void refresh(uint16_t xOffset, uint16_t yOffset) override;
 private:
   Adafruit_ST7789* display;
   GFXcanvas16* canvasSpectrumScope;
   GFXcanvas16* canvasWaterFall;
-  uint16_t signalsData[SSWA_WIDTH];
-  uint16_t noiseData[SSWA_WIDTH];
-  uint16_t signalIndexes[SSWA_WIDTH];
-  // generate random noise data
-  void refreshNoise(void);
-  // generate start/default signals levels
-  void initSignals(void);
-  // regenerate signals levels
-  void refreshSignals(void);
   // move canvas one line down
   void scrollDownWaterFallCanvas(GFXcanvas16* canvas);
   // generate "blue gradient color" from signal (range value 0..255)
@@ -33,4 +23,4 @@ private:
   void draw(uint16_t xOffset, uint16_t yOffset);
 };
 
-#endif  // SDR_REMOTE_CONTROL_SPECTRUM_SCOPE_WATERFALL_ANIMATION_H
+#endif
