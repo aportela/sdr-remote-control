63be00dc03f0e19d79a8eb63c9d1967eaa5e55f6
alex <766f6964+github@gmail.com>
2024-06-12 10:59:59 +0200

    refactored preprocessor debug constant

--- src/display/ST7789/DisplayST7789.cpp
+++ src/display/ST7789/DisplayST7789.cpp
@@ -14,33 +14,38 @@
   In theory the hardware pins should be more efficient but if I use them by calling the constructor that does not use the default pins used in Adafruit, the performance drops considerably
 */
 DisplayST7789::DisplayST7789(uint16_t width, uint16_t height, uint8_t rotation, int8_t pinCS, int8_t pinDC, int8_t pinMOSI, int8_t pinSCLK, int8_t pinRST)
-  : screen(pinCS, pinDC, pinRST) {
+    : screen(pinCS, pinDC, pinRST)
+{
   this->width = width;
   this->height = height;
   this->screen.init(width, height);
-  if (rotation > 0 && rotation < 4) {
+  if (rotation > 0 && rotation < 4)
+  {
     this->screen.setRotation(rotation);
-    if (rotation == 1 || rotation == 3) {
+    if (rotation == 1 || rotation == 3)
+    {
       this->width = height;
       this->height = width;
     }
   }
   this->screen.setTextWrap(false);
   this->screen.fillScreen(ST77XX_BLACK);
-#ifdef DEBUG_FPS
+#ifdef DISPLAY_DEBUG
   this->fpsDebug = new FPSDebug();
 #endif
 }
 
-
-DisplayST7789::~DisplayST7789() {
+DisplayST7789::~DisplayST7789()
+{
 }
 
-void DisplayST7789::clearScreen(uint8_t color) {
+void DisplayST7789::clearScreen(uint8_t color)
+{
   this->screen.fillScreen(color);
 }
 
-void DisplayST7789::showConnectScreen(uint16_t serialBaudRate, float currentVersion) {
+void DisplayST7789::showConnectScreen(uint16_t serialBaudRate, float currentVersion)
+{
   this->screen.fillScreen(ST77XX_BLACK);
   this->screen.drawRect(0, 0, this->width, this->height, ST77XX_WHITE);
   this->screen.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
@@ -64,14 +69,16 @@ void DisplayST7789::showConnectScreen(uint16_t serialBaudRate, float currentVers
   this->animatedScreenPtr = new SSWAnimationST7789(&this->screen, 160);
 }
 
-void DisplayST7789::hideConnectScreen(void) {
+void DisplayST7789::hideConnectScreen(void)
+{
   delete this->animatedScreenPtr;
   this->animatedScreenPtr = nullptr;
 }
 
-void DisplayST7789::refreshConnectScreen() {
+void DisplayST7789::refreshConnectScreen()
+{
   this->animatedScreenPtr->refresh(80, 80);
-#ifdef DEBUG_FPS
+#ifdef DISPLAY_DEBUG
   this->fpsDebug->loop();
   this->screen.setCursor(140, 190);
   this->screen.setTextSize(1);
@@ -79,34 +86,41 @@ void DisplayST7789::refreshConnectScreen() {
 #endif
 }
 
-void DisplayST7789::showMainScreen() {
+void DisplayST7789::showMainScreen()
+{
   this->screen.fillScreen(ST77XX_BLACK);
   this->createDigitalSMeter();
 }
 
-void DisplayST7789::refreshMainScreen(Transceiver* trx) {
-  if (trx->changed & TRX_CFLAG_TRANSMIT_RECEIVE_POWER_STATUS) {
+void DisplayST7789::refreshMainScreen(Transceiver *trx)
+{
+  if (trx->changed & TRX_CFLAG_TRANSMIT_RECEIVE_POWER_STATUS)
+  {
     this->refreshTransmitStatus(false);
     trx->changed &= ~TRX_CFLAG_TRANSMIT_RECEIVE_POWER_STATUS;
   }
-  if (trx->changed & TRX_CFLAG_ACTIVE_VFO_INDEX) {
+  if (trx->changed & TRX_CFLAG_ACTIVE_VFO_INDEX)
+  {
     this->refreshActiveVFO(trx->activeVFOIndex);
     trx->changed &= ~TRX_CFLAG_ACTIVE_VFO_INDEX;
   }
-  if (trx->changed & TRX_CFLAG_ACTIVE_VFO_MODE) {
+  if (trx->changed & TRX_CFLAG_ACTIVE_VFO_MODE)
+  {
     this->refreshVFOMode(trx->VFO[trx->activeVFOIndex].mode);
     trx->changed &= ~TRX_CFLAG_ACTIVE_VFO_MODE;
   }
-  if (trx->changed & TRX_CFLAG_ACTIVE_VFO_FREQUENCY) {
+  if (trx->changed & TRX_CFLAG_ACTIVE_VFO_FREQUENCY)
+  {
     this->refreshVFOFreq(trx->VFO[trx->activeVFOIndex].frequency);
     trx->changed &= ~TRX_CFLAG_ACTIVE_VFO_FREQUENCY;
   }
-  if (trx->changed & TRX_CFLAG_SIGNAL_METER_LEVEL) {
-    //this->refreshRNDDigitalSMeter(random(0, 42));
+  if (trx->changed & TRX_CFLAG_SIGNAL_METER_LEVEL)
+  {
+    // this->refreshRNDDigitalSMeter(random(0, 42));
     this->refreshRNDDigitalSMeter(trx->signalMeterLevel);
-    //trx->changed &= ~TRX_CFLAG_SIGNAL_METER_LEVEL;
+    // trx->changed &= ~TRX_CFLAG_SIGNAL_METER_LEVEL;
   }
-#ifdef DEBUG_FPS
+#ifdef DISPLAY_DEBUG
   this->fpsDebug->loop();
   this->refreshFPS(this->fpsDebug->getFPS());
 #endif
@@ -157,11 +171,15 @@ void DisplayST7789::refreshMainScreen(sdrRemoteTransceiver* trx) {
 }
 
 */
-void DisplayST7789::refreshTransmitStatus(bool isTransmitting) {
-  if (isTransmitting) {
+void DisplayST7789::refreshTransmitStatus(bool isTransmitting)
+{
+  if (isTransmitting)
+  {
     this->screen.drawRect(0, 0, 29, 20, ST77XX_RED);
     this->screen.setTextColor(ST77XX_RED, ST77XX_BLACK);
-  } else {
+  }
+  else
+  {
     this->screen.drawRect(0, 0, 29, 20, ST77XX_GREEN);
     this->screen.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
   }
@@ -170,7 +188,8 @@ void DisplayST7789::refreshTransmitStatus(bool isTransmitting) {
   this->screen.print(isTransmitting ? "TX" : "RX");
 }
 
-void DisplayST7789::refreshActiveVFO(uint8_t number) {
+void DisplayST7789::refreshActiveVFO(uint8_t number)
+{
   this->screen.drawRect(31, 0, 53, 20, ST77XX_WHITE);
   this->screen.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
   this->screen.setCursor(34, 3);
@@ -178,49 +197,52 @@ void DisplayST7789::refreshActiveVFO(uint8_t number) {
   this->screen.printf("VFO%d", number);
 }
 
-void DisplayST7789::refreshVFOMode(TRXVFOMode mode) {
+void DisplayST7789::refreshVFOMode(TRXVFOMode mode)
+{
   this->screen.drawRect(86, 0, 43, 20, ST77XX_WHITE);
   this->screen.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
   this->screen.setCursor(89, 3);
   this->screen.setTextSize(2);
-  switch (mode) {
-    case TRX_VFO_MD_DSB:
-      this->screen.printf("DSB");
-      break;
-    case TRX_VFO_MD_LSB:
-      this->screen.printf("LSB");
-      break;
-    case TRX_VFO_MD_USB:
-      this->screen.printf("USB");
-      break;
-    case TRX_VFO_MD_CW_U:
-      this->screen.printf("CWU");  // CW (upper sideband)
-      break;
-    case TRX_VFO_MD_FM:
-      this->screen.printf(" FM");
-      break;
-    case TRX_VFO_MD_SAM:
-      this->screen.printf("SAM");  // (synchronous AM, includes ECSS)
-      break;
-    case TRX_VFO_MD_RESERVED:
-      this->screen.printf("  ");  // reserved / not used
-      break;
-    case TRX_VFO_MD_CW_L:
-      this->screen.printf("CWL");  // CW (lower sideband)
-      break;
-    case TRX_VFO_MD_WFM:
-      this->screen.printf("WFM");
-      break;
-    case TRX_VFO_MD_BFM:
-      this->screen.printf("BFM");
-      break;
-    default:
-      this->screen.printf("???");  // error / invalid value
-      break;
+  switch (mode)
+  {
+  case TRX_VFO_MD_DSB:
+    this->screen.printf("DSB");
+    break;
+  case TRX_VFO_MD_LSB:
+    this->screen.printf("LSB");
+    break;
+  case TRX_VFO_MD_USB:
+    this->screen.printf("USB");
+    break;
+  case TRX_VFO_MD_CW_U:
+    this->screen.printf("CWU"); // CW (upper sideband)
+    break;
+  case TRX_VFO_MD_FM:
+    this->screen.printf(" FM");
+    break;
+  case TRX_VFO_MD_SAM:
+    this->screen.printf("SAM"); // (synchronous AM, includes ECSS)
+    break;
+  case TRX_VFO_MD_RESERVED:
+    this->screen.printf("  "); // reserved / not used
+    break;
+  case TRX_VFO_MD_CW_L:
+    this->screen.printf("CWL"); // CW (lower sideband)
+    break;
+  case TRX_VFO_MD_WFM:
+    this->screen.printf("WFM");
+    break;
+  case TRX_VFO_MD_BFM:
+    this->screen.printf("BFM");
+    break;
+  default:
+    this->screen.printf("???"); // error / invalid value
+    break;
   }
 }
 
-void DisplayST7789::refreshFPS(uint16_t fps) {
+void DisplayST7789::refreshFPS(uint16_t fps)
+{
   this->screen.drawRect(230, 0, 89, 20, 0xF85E);
   this->screen.setTextColor(0xF85E, ST77XX_BLACK);
   this->screen.setCursor(233, 3);
@@ -228,7 +250,8 @@ void DisplayST7789::refreshFPS(uint16_t fps) {
   this->screen.printf("%03u FPS", fps);
 }
 
-void DisplayST7789::refreshVFOFreq(uint64_t frequency) {
+void DisplayST7789::refreshVFOFreq(uint64_t frequency)
+{
   this->screen.setTextSize(3);
   char formattedFrequency[16];
   char nformattedFrequency[12];
@@ -245,9 +268,11 @@ void DisplayST7789::refreshVFOFreq(uint64_t frequency) {
   // test if this method is more optimized than div operators
   int resultIndex = 0;
   sprintf(nformattedFrequency, "%012llu", frequency);
-  for (int i = 0; i < 12; ++i) {
+  for (int i = 0; i < 12; ++i)
+  {
     formattedFrequency[resultIndex++] = nformattedFrequency[i];
-    if ((i + 1) % 3 == 0 && i < 11) {
+    if ((i + 1) % 3 == 0 && i < 11)
+    {
       formattedFrequency[resultIndex++] = '.';
     }
   }
@@ -259,7 +284,8 @@ void DisplayST7789::refreshVFOFreq(uint64_t frequency) {
   this->screen.print(formattedFrequency);
 }
 
-void DisplayST7789::createDigitalSMeter() {
+void DisplayST7789::createDigitalSMeter()
+{
   randomSeed(analogRead(0));
   this->screen.setTextSize(2);
   this->screen.setTextColor(ST77XX_WHITE);
@@ -275,50 +301,70 @@ void DisplayST7789::createDigitalSMeter() {
   this->screen.drawFastVLine(284, 94, 22, ST77XX_WHITE);
   this->screen.drawFastHLine(23, 116, 262, ST77XX_WHITE);
 
-  for (int i = 0; i <= SMETER_PARTS; i++) {
+  for (int i = 0; i <= SMETER_PARTS; i++)
+  {
     int x = 26 + i * (SMETER_PART_WIDTH + SMETER_PART_SPACE_WIDTH);
-    if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 26 || i == 34 || i == 42) {
+    if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 26 || i == 34 || i == 42)
+    {
       this->screen.fillRect(x, 94, SMETER_PART_WIDTH, 22, SMETER_PARTH_BG_COLOR);
-    } else {
+    }
+    else
+    {
       this->screen.fillRect(x, 98, SMETER_PART_WIDTH, SMETER_PARTH_HEIGHT, SMETER_PARTH_BG_COLOR);
     }
   }
 }
 
-void DisplayST7789::refreshRNDDigitalSMeter(int newSignal) {
+void DisplayST7789::refreshRNDDigitalSMeter(int newSignal)
+{
   this->oldSignal = this->currentSignal;
   // signal changed
-  if (this->oldSignal != newSignal) {
-    if (newSignal >= this->oldSignal) {
+  if (this->oldSignal != newSignal)
+  {
+    if (newSignal >= this->oldSignal)
+    {
       // signal increased
       this->currentSignal = newSignal;
-      if (newSignal > this->oldSignal) {
+      if (newSignal > this->oldSignal)
+      {
         this->peakSignal = newSignal;
         this->lastPeakChange = millis();
       }
-    } else if (newSignal < this->oldSignal) {
-      //this->peakSignal = this->oldSignal;
+    }
+    else if (newSignal < this->oldSignal)
+    {
+      // this->peakSignal = this->oldSignal;
       long current = millis();
-      if (current - this->lastPeakChange > 200) {
+      if (current - this->lastPeakChange > 200)
+      {
         this->peakSignal--;
         this->lastPeakChange = current;
       }
       this->currentSignal = this->oldSignal - 1;
     }
-    if (this->currentSignal < 0) {
+    if (this->currentSignal < 0)
+    {
       this->currentSignal = 0;
-    } else if (this->currentSignal > 42) {
+    }
+    else if (this->currentSignal > 42)
+    {
       this->currentSignal = 42;
     }
     int start = 0;
-    if (this->oldSignal < this->currentSignal) {
+    if (this->oldSignal < this->currentSignal)
+    {
       start = this->oldSignal;
-    } else if (this->oldSignal > this->currentSignal) {
+    }
+    else if (this->oldSignal > this->currentSignal)
+    {
       start = this->currentSignal;
-    } else {
+    }
+    else
+    {
       start = 0;
     }
-    if (this->peakSignal < 0) {
+    if (this->peakSignal < 0)
+    {
       this->peakSignal = 0;
     }
 
@@ -339,66 +385,94 @@ void DisplayST7789::refreshRNDDigitalSMeter(int newSignal) {
     */
 
     // BG
-    for (int i = start; i <= SMETER_PARTS; i++) {
+    for (int i = start; i <= SMETER_PARTS; i++)
+    {
       int x = 26 + i * (SMETER_PART_WIDTH + SMETER_PART_SPACE_WIDTH);
-      if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42) {
+      if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42)
+      {
         this->screen.fillRect(x, 94, SMETER_PART_WIDTH, 22, SMETER_PARTH_BG_COLOR);
-      } else {
+      }
+      else
+      {
         this->screen.fillRect(x, 98, SMETER_PART_WIDTH, SMETER_PARTH_HEIGHT, SMETER_PARTH_BG_COLOR);
       }
     }
 
     // PEAK
-    for (int i = 0; i <= SMETER_PARTS; i++) {
-      if (i == this->peakSignal) {
+    for (int i = 0; i <= SMETER_PARTS; i++)
+    {
+      if (i == this->peakSignal)
+      {
         int x = 26 + i * (SMETER_PART_WIDTH + SMETER_PART_SPACE_WIDTH);
         // set more height on marked values
-        if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42) {
+        if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42)
+        {
           this->screen.fillRect(x, 94, SMETER_PART_WIDTH, 22, ST77XX_WHITE);
-        } else {
+        }
+        else
+        {
           this->screen.fillRect(x, 98, SMETER_PART_WIDTH, SMETER_PARTH_HEIGHT, ST77XX_WHITE);
         }
       }
     }
 
     // SIGNAL
-    for (int i = 0; i <= SMETER_PARTS; i++) {
+    for (int i = 0; i <= SMETER_PARTS; i++)
+    {
       // real value draw
-      if (i < this->currentSignal) {
+      if (i < this->currentSignal)
+      {
         int x = 26 + i * (SMETER_PART_WIDTH + SMETER_PART_SPACE_WIDTH);
         // set more height on marked values
-        if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42) {
+        if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42)
+        {
           this->screen.fillRect(x, 94, SMETER_PART_WIDTH, 22, i <= 18 ? ST77XX_GREEN : ST77XX_RED);
-        } else {
+        }
+        else
+        {
           this->screen.fillRect(x, 98, SMETER_PART_WIDTH, SMETER_PARTH_HEIGHT, i <= 18 ? ST77XX_GREEN : ST77XX_RED);
         }
       }
     }
-  } else {
-    if (this->peakSignal > this->currentSignal) {
+  }
+  else
+  {
+    if (this->peakSignal > this->currentSignal)
+    {
       long current = millis();
-      if (current - this->lastPeakChange > 200) {
+      if (current - this->lastPeakChange > 200)
+      {
         this->peakSignal--;
-        if (this->peakSignal > 0) {
+        if (this->peakSignal > 0)
+        {
           this->peakSignal--;
           this->lastPeakChange = current;
 
-          for (int i = this->peakSignal + 1; i <= SMETER_PARTS; i++) {
+          for (int i = this->peakSignal + 1; i <= SMETER_PARTS; i++)
+          {
             int x = 26 + i * (SMETER_PART_WIDTH + SMETER_PART_SPACE_WIDTH);
-            if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42) {
+            if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42)
+            {
               this->screen.fillRect(x, 94, SMETER_PART_WIDTH, 22, SMETER_PARTH_BG_COLOR);
-            } else {
+            }
+            else
+            {
               this->screen.fillRect(x, 98, SMETER_PART_WIDTH, SMETER_PARTH_HEIGHT, SMETER_PARTH_BG_COLOR);
             }
           }
 
-          for (int i = 0; i <= SMETER_PARTS; i++) {
-            if (i == this->peakSignal) {
+          for (int i = 0; i <= SMETER_PARTS; i++)
+          {
+            if (i == this->peakSignal)
+            {
               int x = 26 + i * (SMETER_PART_WIDTH + SMETER_PART_SPACE_WIDTH);
               // set more height on marked values
-              if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42) {
+              if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 22 || i == 26 || i == 30 || i == 34 || i == 38 || i == 42)
+              {
                 this->screen.fillRect(x, 94, SMETER_PART_WIDTH, 22, ST77XX_WHITE);
-              } else {
+              }
+              else
+              {
                 this->screen.fillRect(x, 98, SMETER_PART_WIDTH, SMETER_PARTH_HEIGHT, ST77XX_WHITE);
               }
             }
