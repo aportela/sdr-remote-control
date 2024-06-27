6bf65638b0046139727dc782a0f2dee566636028
alex <766f6964+github@gmail.com>
2024-06-27 12:47:58 +0200

    fixed virtual methods

--- src/display/IMenu.hpp
+++ src/display/IMenu.hpp
@@ -7,9 +7,9 @@ class IMenu
 {
 public:
     virtual ~IMenu() {}
-    virtual void refresh(uint8_t startMenuIndex, uint8_t selectedMenuIndex);
-    virtual void previousPage(void);
-    virtual void nextPage(void);
+    virtual void refresh(uint8_t startMenuIndex, uint8_t selectedMenuIndex) = 0;
+    virtual void previousPage(void) = 0;
+    virtual void nextPage(void) = 0;
 };
 
 #endif
