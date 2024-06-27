55638d8edb95e34b7961cd99199664cb682f3fc1
alex <766f6964+github@gmail.com>
2024-06-27 13:07:55 +0200

    changed colors & aligments

--- src/display/ILI9488/MenuButton-ILI9488.cpp
+++ src/display/ILI9488/MenuButton-ILI9488.cpp
@@ -1,7 +1,7 @@
 #include "MenuButton-ILI9488.hpp"
 
 #define BUTTON_WIDTH 111
-#define BUTTON_HEIGHT 48
+#define BUTTON_HEIGHT 46
 
 #define BUTTON_NOT_ACTIVE_BACKGROUND_COLOR 0x0000
 #define BUTTON_ACTIVE_BACKGROUND_COLOR 0xFFFF
@@ -18,13 +18,18 @@ MenuButtonILI9488::MenuButtonILI9488(LGFX *existingDisplay, uint8_t index, uint1
     }
 }
 
+MenuButtonILI9488::~MenuButtonILI9488()
+{
+    this->display = nullptr;
+}
+
 void MenuButtonILI9488::draw(void)
 {
     if (this->changed)
     {
         if (!this->active)
         {
-            this->display->fillRect(this->x, this->y, this->width, this->height, BUTTON_NOT_ACTIVE_BACKGROUND_COLOR);
+            this->display->drawRect(this->x, this->y, this->width, this->height, BUTTON_ACTIVE_BACKGROUND_COLOR);
         }
         else
         {
@@ -39,9 +44,9 @@ void MenuButtonILI9488::draw(void)
         {
             this->display->setTextColor(BUTTON_ACTIVE_LABEL_COLOR, BUTTON_ACTIVE_BACKGROUND_COLOR);
         }
-        this->display->setCursor(this->x + 42, this->y + 5);
+        this->display->setCursor(this->x + 44, this->y + 5);
         this->display->print(this->topLabel);
-        this->display->setCursor(this->x + 9, this->y + 28);
+        this->display->setCursor(this->x + 8, this->y + 26);
         this->display->print(this->bottomLabel);
         this->changed = false;
     }
