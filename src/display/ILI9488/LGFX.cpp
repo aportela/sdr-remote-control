4af1f4f52c068af5b4a884e16e1d1f689b5a50ec
alex <766f6964+github@gmail.com>
2024-09-18 12:16:46 +0200

    updated main LGFX class

--- src/display/ILI9488/LGFX.cpp
+++ src/display/ILI9488/LGFX.cpp
@@ -1,44 +1,135 @@
 #include "LGFX.hpp"
+#include "LGFXScreenConnecting.hpp"
+#include "../SizesAndOffsets-320x240.hpp"
 
-LGFX::LGFX(void) {
-  {
+LGFX::LGFX(uint8_t PIN_SDA, uint8_t PIN_SCL, uint8_t PIN_CS, uint8_t PIN_DC, uint8_t PIN_RST, uint16_t width, uint16_t height, uint8_t rotation, bool invertColors = true)
+{
     auto cfg = _bus_instance.config();
-    cfg.spi_host = VSPI_HOST;   // Utilizar el SPI del hardware VSPI
-    cfg.spi_mode = 0;           // Modo SPI (0, 1, 2, 3)
-    cfg.freq_write = 40000000;  // Frecuencia de escritura
-    cfg.freq_read = 16000000;   // Frecuencia de lectura
-    cfg.spi_3wire = false;      // No utilizar modo de 3 hilos
-    cfg.use_lock = true;        // Utilizar bloqueo durante las transacciones
-    cfg.dma_channel = 1;        // Canal DMA (0=Sin uso, 1=Canal 1, 2=Canal 2)
-    cfg.pin_sclk = 18;          // Pin de reloj SCK
-    cfg.pin_mosi = 23;          // Pin MOSI
-    cfg.pin_miso = 19;          // Pin MISO
-    cfg.pin_dc = 2;             // Pin de datos/comando DC
+    cfg.spi_host = VSPI_HOST;
+    cfg.spi_mode = 0;
+    cfg.freq_write = 40000000;
+    cfg.freq_read = 16000000;
+    cfg.spi_3wire = false;
+    cfg.use_lock = true;
+    cfg.dma_channel = 1;
+    cfg.pin_sclk = PIN_SCL;
+    cfg.pin_mosi = PIN_SDA;
+    cfg.pin_miso = -1;
+    cfg.pin_dc = PIN_DC;
+    _bus_instance.config(cfg);
+    _panel_instance.setBus(&_bus_instance);
 
-    _bus_instance.config(cfg);               // Aplicar configuración
-    _panel_instance.setBus(&_bus_instance);  // Asignar el bus al panel
-  }
+    auto cfg_panel = _panel_instance.config();
+    cfg_panel.pin_cs = PIN_CS;
+    cfg_panel.pin_rst = PIN_RST;
+    cfg_panel.pin_busy = -1;
+    cfg_panel.memory_width = width;
+    cfg_panel.memory_height = height;
+    cfg_panel.panel_width = width;
+    cfg_panel.panel_height = height;
+    cfg_panel.offset_x = 0;
+    cfg_panel.offset_y = 0;
+    cfg_panel.offset_rotation = rotation;
+    cfg_panel.dummy_read_pixel = 8;
+    cfg_panel.dummy_read_bits = 1;
+    cfg_panel.readable = true;
+    cfg_panel.invert = true;
+    cfg_panel.rgb_order = false;
+    cfg_panel.dlen_16bit = false;
+    cfg_panel.bus_shared = true;
+    _panel_instance.config(cfg_panel);
 
-  {  // Configuración del panel
-    auto cfg = _panel_instance.config();
-    cfg.pin_cs = 15;    // Pin Chip Select
-    cfg.pin_rst = 4;    // Pin de reinicio
-    cfg.pin_busy = -1;  // Pin de ocupado (si no se utiliza, poner -1)
+    setPanel(&_panel_instance);
 
-    cfg.panel_width = 320;     // Ancho del panel
-    cfg.panel_height = 480;    // Altura del panel
-    cfg.offset_x = 0;          // Desplazamiento en X
-    cfg.offset_y = 0;          // Desplazamiento en Y
-    cfg.offset_rotation = 1;   // Rotación de la pantalla
-    cfg.dummy_read_pixel = 8;  // Lecturas dummy por pixel
-    cfg.dummy_read_bits = 1;   // Lecturas dummy por bits
-    cfg.readable = true;       // La pantalla puede ser leída
-    cfg.invert = true;         // No invertir los colores
-    cfg.rgb_order = false;     // Orden de color RGB
-    cfg.dlen_16bit = false;    // Longitud de datos de 16 bits
-    cfg.bus_shared = true;     // El bus es compartido con otros dispositivos
+    this->init();
+    this->fillScreen(TFT_BLACK);
+}
 
-    _panel_instance.config(cfg);  // Aplicar configuración
-  }
-  setPanel(&_panel_instance);  // Asignar el panel al dispositivo
-};
+LGFX::~LGFX()
+{
+    if (this->currentScreen != nullptr)
+    {
+        delete this->currentScreen;
+        this->currentScreen = nullptr;
+    }
+}
+
+void LGFX::InitScreen(SCREEN_TYPE screenType)
+{
+    switch (screenType)
+    {
+    case SCREEN_TYPE_NOT_CONNECTED:
+        if (this->currentScreen == nullptr)
+        {
+            Serial.println("SI");
+            this->currentScreen = new LGFXScreenConnecting(this);
+        }
+        this->currentScreenType = screenType;
+        break;
+    case SCREEN_TYPE_NONE:
+    default:
+        break;
+    }
+}
+
+void LGFX::DeleteCurrentScreen(void)
+{
+    if (this->currentScreenType != SCREEN_TYPE_NONE)
+    {
+        if (this->currentScreen != nullptr)
+        {
+            delete this->currentScreen;
+            this->currentScreen = nullptr;
+            this->currentScreenType == SCREEN_TYPE_NONE;
+        }
+    }
+}
+
+SCREEN_TYPE LGFX::GetCurrentScreenType(void)
+{
+    return (this->currentScreenType);
+}
+
+bool LGFX::FlipToScreen(SCREEN_TYPE screenType)
+{
+    if (screenType != this->currentScreenType)
+    {
+        this->DeleteCurrentScreen();
+        this->InitScreen(screenType);
+        return (true);
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFX::ToggleScreen(void)
+{
+    bool success = false;
+    switch (this->GetCurrentScreenType())
+    {
+    default:
+        break;
+    }
+    return (success);
+}
+
+bool LGFX::Refresh(void)
+{
+    if (this->currentScreenType != SCREEN_TYPE_NONE)
+    {
+        if (this->currentScreen != nullptr)
+        {
+            return (this->currentScreen->Refresh(false));
+        }
+        else
+        {
+            return (false);
+        }
+    }
+    else
+    {
+        return (false);
+    }
+}
--- src/display/ILI9488/LGFX.hpp
+++ src/display/ILI9488/LGFX.hpp
@@ -2,12 +2,30 @@
 #define SDR_REMOTE_CONTROL_LGFX_H
 
 #include <LovyanGFX.hpp>
+#include <stdint.h>
+#include "LGFXScreen.hpp"
+#include "../ScreenType.hpp"
 
-class LGFX : public lgfx::LGFX_Device {
-  lgfx::Panel_ILI9488 _panel_instance;
-  lgfx::Bus_SPI _bus_instance;
+class LGFX : public lgfx::LGFX_Device
+{
+private:
+    lgfx::Panel_ST7789 _panel_instance;
+    lgfx::Bus_SPI _bus_instance;
+
+    SCREEN_TYPE currentScreenType = SCREEN_TYPE_NONE;
+
+    LGFXScreen *currentScreen = nullptr;
+
+    void DeleteCurrentScreen(void);
+
 public:
-  LGFX(void);
+    LGFX(uint8_t PIN_SDA, uint8_t PIN_SCL, uint8_t PIN_CS, uint8_t PIN_DC, uint8_t PIN_RST, uint16_t width, uint16_t height, uint8_t rotation, bool invertColors);
+    ~LGFX();
+    void InitScreen(SCREEN_TYPE scr);
+    SCREEN_TYPE GetCurrentScreenType(void);
+    bool FlipToScreen(SCREEN_TYPE scr);
+    bool ToggleScreen(void);
+    bool Refresh(void);
 };
 
-#endif
+#endif // SDR_REMOTE_CONTROL_LGFX_H
