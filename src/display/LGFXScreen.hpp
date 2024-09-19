723eea3ac86e0415e6809458f6f4b74368fa27dd
alex <766f6964+github@gmail.com>
2024-09-19 08:47:06 +0200

    added FPS protected property

--- src/display/LGFXScreen.hpp
+++ src/display/LGFXScreen.hpp
@@ -3,11 +3,15 @@
 
 #include <LovyanGFX.hpp>
 #include "IScreen.hpp"
+#include "../DisplayConfiguration.hpp"
 
 class LGFXScreen : public IScreen
 {
 protected:
     LovyanGFX *parentDisplay = nullptr;
+#ifdef DEBUG_FPS
+    uint16_t previousFPS = 0;
+#endif
 
 public:
     LGFXScreen(LovyanGFX *display);
