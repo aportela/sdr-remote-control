be17de9f4eabba5f8b9b436d41d0b76a0221c0b3
alex <766f6964+github@gmail.com>
2024-09-23 12:32:23 +0200

    old menu refactored

--- src/display/Menu.hpp
+++ src/display/Menu.hpp
@@ -1,18 +1,10 @@
 #ifndef SDR_REMOTE_CONTROL_MENU_H
 #define SDR_REMOTE_CONTROL_MENU_H
 
-#include <Arduino.h>
 #include "IMenu.hpp"
 
 class Menu : public IMenu
 {
-public:
-    Menu(void);
-    virtual void refresh(bool forceAll) = 0;
-    virtual void previousPage(void) = 0;
-    virtual void nextPage(void) = 0;
-
-private:
 protected:
     uint16_t width;
     uint16_t height;
@@ -20,5 +12,11 @@ protected:
     uint16_t yOffset;
     uint8_t currentPage;
     uint8_t totalPages;
+
+public:
+    Menu(void);
+    virtual void refresh(bool forceAll) = 0;
+    virtual void previousPage(void) = 0;
+    virtual void nextPage(void) = 0;
 };
 #endif
