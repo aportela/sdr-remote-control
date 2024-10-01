351122686d9a60ba891b45b900b0414a127afb7c
alex <766f6964+github@gmail.com>
2024-10-01 12:27:12 +0200

    removed unwanted includes

--- src/connection/SDRRadio-TS2K/SDRRadioTS2KSerialConnection.hpp
+++ src/connection/SDRRadio-TS2K/SDRRadioTS2KSerialConnection.hpp
@@ -1,8 +1,6 @@
 #ifndef SDR_REMOTE_CONTROL_SDRRADIO_TS2K_SERIAL_CONNECTION_H
 #define SDR_REMOTE_CONTROL_SDRRADIO_TS2K_SERIAL_CONNECTION_H
 
-#include <Arduino.h>
-#include <String.h>
 #include "../SerialConnection.hpp"
 
 class SDRRadioTS2KSerialConnection : public SerialConnection
