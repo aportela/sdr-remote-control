07514db4adfcea3c538177dcd34a79983bfd52d9
alex <766f6964+github@gmail.com>
2024-10-07 12:08:13 +0200

    added dummy connection for display testing

--- sdr-remote-control.ino
+++ sdr-remote-control.ino
@@ -11,28 +11,27 @@
 #include "src/controls/RotaryControl.hpp"
 #include "src/Transceiver.hpp"
 
+#define DEBUG_DUMMY_CONNECTION
+
 #ifdef DEBUG_FPS
-
 #include "src/utils/FPS.hpp"
-
 #endif
 
 using namespace aportela::microcontroller::utils;
 
+#ifdef DEBUG_DUMMY_CONNECTION
+#include "src/connection/dummy/DummyConnection.hpp"
+#else
 #include "src/connection/SDRRadio-TS2K/SDRRadioTS2KSerialConnection.hpp"
+#endif
 
 #include "src/display/ScreenType.hpp"
 
 #ifdef DISPLAY_DRIVER_LOVYANN
-
 #include "src/display/LGFX.hpp"
-
 LGFX *screen = nullptr;
-
 #else
-
 #error NO DISPLAY DRIVER DEFINED
-
 #endif // DISPLAY_DRIVER_LOVYANN
 
 Transceiver *trx = nullptr;
@@ -56,7 +55,11 @@ volatile uint8_t encoderChangeBitmask = 0;
 
 uint64_t lastEncoderMillis = 0;
 
-SDRRadioTS2KSerialConnection *serialConnection;
+#ifdef DEBUG_DUMMY_CONNECTION
+DummyConnection *connection;
+#else
+SDRRadioTS2KSerialConnection *connection;
+#endif
 
 void onEncoderIncrement(uint8_t acceleratedDelta = 1, uint64_t lastMillis = 0)
 {
@@ -108,28 +111,22 @@ void onEncoderDecrement(uint8_t acceleratedDelta = 1, uint64_t lastMillis = 0)
 
 void setup()
 {
-  /*
-  Serial.begin(SERIAL_DEFAULT_BAUD_RATE);
-  while (!Serial)
-  {
-    yield();
-    delay(10);
-  }
-  */
-  serialConnection = new SDRRadioTS2KSerialConnection(&Serial, SERIAL_DEFAULT_BAUD_RATE, SERIAL_TIMEOUT);
+#ifdef DEBUG_DUMMY_CONNECTION
+  connection = new DummyConnection(&Serial, SERIAL_DEFAULT_BAUD_RATE, SERIAL_TIMEOUT);
+#else
+  connection = new SDRRadioTS2KSerialConnection(&Serial, SERIAL_DEFAULT_BAUD_RATE, SERIAL_TIMEOUT);
+#endif
+
   RotaryControl::init(ENC1_A, ENC1_B, onEncoderIncrement, onEncoderDecrement);
   encoderChangeBitmask |= ENCODER_CHANGE_TUNE;
   trx = new Transceiver();
   trxStatus = new TransceiverStatus;
-#ifdef DISPLAY_DRIVER_LOVYANN
 
+#ifdef DISPLAY_DRIVER_LOVYANN
   screen = new LGFX(DISPLAY_PIN_SDA, DISPLAY_PIN_SCL, DISPLAY_PIN_CS, DISPLAY_PIN_DC, DISPLAY_PIN_RST, DISPLAY_DRIVER_LOVYANN_WIDTH, DISPLAY_DRIVER_LOVYANN_HEIGHT, DISPLAY_DRIVER_LOVYANN_ROTATION, DISPLAY_DRIVER_LOVYANN_INVERT_COLORS, trxStatus);
   screen->InitScreen(SCREEN_TYPE_NOT_CONNECTED);
-
 #else
-
 #error NO DISPLAY DRIVER DEFINED
-
 #endif // DISPLAY_DRIVER_LOVYANN
 }
 
@@ -139,7 +136,7 @@ void loop()
   {
     if (!trxStatus->poweredOn)
     {
-      if (serialConnection->tryConnection(trx))
+      if (connection->tryConnection(trx))
       {
         trx->setPowerOnStatus(true);
         screen->FlipToScreen(SCREEN_TYPE_CONNECTED);
@@ -147,15 +144,19 @@ void loop()
     }
     else
     {
-      if (serialConnection->isDisconnectedByTimeout())
+#ifdef DEBUG_DUMMY_CONNECTION
+      connection->loop(trx, trxStatus);
+#else
+      if (connection->isDisconnectedByTimeout())
       {
         trx->setPowerOnStatus(false);
         screen->FlipToScreen(SCREEN_TYPE_NOT_CONNECTED);
       }
       else
       {
-        serialConnection->loop(trx, trxStatus);
+        connection->loop(trx, trxStatus);
       }
+#endif
     }
   }
 #ifdef DISPLAY_DRIVER_LOVYANN
