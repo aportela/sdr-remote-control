fa4d28aac23d8aa58f8ce08488d699d14efb210b
alex <766f6964+github@gmail.com>
2024-10-30 11:50:34 +0100

    background color changed

--- src/display/widgets/LGFXBandWidget.cpp
+++ src/display/widgets/LGFXBandWidget.cpp
@@ -1,6 +1,7 @@
 #include "LGFXBandWidget.hpp"
 
-#define TEXT_COLOR_MUTED TFT_RED
+#define _BAND_WIDGET_BACKGROUND_COLOR 0x19cc
+#define _BAND_WIDGET_BORDER_COLOR TEXT_COLOR_ACTIVE
 
 LGFXBandWidget::LGFXBandWidget(LovyanGFX *displayPtr, uint16_t width, uint16_t height, uint16_t xOffset, uint16_t yOffset, uint8_t padding, const TransceiverStatus *currentTransceiverStatusPtr) : LGFXTransceiverStatusWidget(displayPtr, width, height, xOffset, yOffset, padding, currentTransceiverStatusPtr)
 {
@@ -11,8 +12,8 @@ LGFXBandWidget::LGFXBandWidget(LovyanGFX *displayPtr, uint16_t width, uint16_t h
     this->buttonBackgroundSprite->createSprite(this->width, this->height);
     this->buttonBackgroundSprite->fillScreen(TFT_BLACK);
 
-    this->buttonBackgroundSprite->fillRoundRect(0, 0, this->width, this->height, _BAND_WIDGET_BACKGROUND_RECT_RADIUS, TEXT_COLOR_NOT_ACTIVE);
-    this->buttonBackgroundSprite->drawRoundRect(0, 0, this->width, this->height, _BAND_WIDGET_BACKGROUND_RECT_RADIUS, TEXT_COLOR_ACTIVE);
+    this->buttonBackgroundSprite->fillRoundRect(0, 0, this->width, this->height, _BAND_WIDGET_BACKGROUND_RECT_RADIUS, _BAND_WIDGET_BACKGROUND_COLOR);
+    this->buttonBackgroundSprite->drawRoundRect(0, 0, this->width, this->height, _BAND_WIDGET_BACKGROUND_RECT_RADIUS, _BAND_WIDGET_BORDER_COLOR);
     if (currentTransceiverStatusPtr != nullptr)
     {
       this->refresh(true);
@@ -33,7 +34,7 @@ void LGFXBandWidget::refreshLabels(uint16_t currentBandIndex)
 {
   this->buttonBackgroundSprite->pushSprite(this->xOffset + this->padding, this->yOffset + this->padding);
 
-  this->parentDisplayPtr->setTextColor(TEXT_COLOR_ACTIVE, TEXT_COLOR_NOT_ACTIVE);
+  this->parentDisplayPtr->setTextColor(TEXT_COLOR_ACTIVE, _BAND_WIDGET_BACKGROUND_COLOR);
   this->parentDisplayPtr->setTextSize(_BAND_WIDGET_MAIN_LABEL_FONT_SIZE);
 
   uint16_t centeredTextXOffset = (this->width - (this->padding * 2) - this->parentDisplayPtr->textWidth(RadioBands[currentBandIndex].label)) / 2;
