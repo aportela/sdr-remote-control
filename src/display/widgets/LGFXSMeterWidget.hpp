e73c214ba14f0f872d74e7301408d30539489f95
alex <766f6964+github@gmail.com>
2024-12-04 11:11:14 +0100

    protected virtual methods

--- src/display/widgets/LGFXSMeterWidget.hpp
+++ src/display/widgets/LGFXSMeterWidget.hpp
@@ -12,9 +12,11 @@ private:
   int8_t previousDBValue = SMETER_MIN_DB;
   int8_t previousDBSmoothedValue = SMETER_MIN_DB;
   lgfx::LGFX_Sprite *dbLabelExpSprite;
+  void refreshLabel(bool force, int8_t dB);
+
+protected:
   virtual void init(void) = 0;
   virtual void update(int8_t dB) = 0;
-  void refreshLabel(bool force, int8_t dB);
 
 public:
   LGFXSMeterWidget(LovyanGFX *displayPtr, uint16_t width, uint16_t height, uint16_t xOffset, uint16_t yOffset, uint8_t padding, const TransceiverStatus *currentTransceiverStatusPtr = nullptr);
