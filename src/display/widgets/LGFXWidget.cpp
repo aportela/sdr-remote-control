a90b4e9b4fe393056405c13499c1f93217c15bba
alex <766f6964+github@gmail.com>
2024-12-10 01:08:38 +0100

    added option for restoring original/previous font (after using custom font)

--- src/display/widgets/LGFXWidget.cpp
+++ src/display/widgets/LGFXWidget.cpp
@@ -2,17 +2,21 @@
 
 LGFXWidget::LGFXWidget(LovyanGFX *displayPtr, uint16_t width, uint16_t height, uint16_t xOffset, uint16_t yOffset, uint8_t padding) : Widget(width, height, xOffset, yOffset, padding), parentDisplayPtr(displayPtr)
 {
-#ifdef DEBUG_SCREEN_WIDGETS_BOUNDS
-
     if (displayPtr != nullptr)
     {
+        this->previousFont = this->parentDisplayPtr->getFont();
+#ifdef DEBUG_SCREEN_WIDGETS_BOUNDS
         this->parentDisplayPtr->drawRect(xOffset, yOffset, width, height, TFT_WHITE);
-    }
-
 #endif // DEBUG_SCREEN_WIDGETS_BOUNDS
+    }
 }
 
 LGFXWidget::~LGFXWidget()
 {
     this->parentDisplayPtr = nullptr;
+}
+
+void LGFXWidget::restorePreviousFont(void)
+{
+    this->parentDisplayPtr->setFont(this->previousFont);
 }
--- src/display/widgets/LGFXWidget.hpp
+++ src/display/widgets/LGFXWidget.hpp
@@ -8,6 +8,9 @@
 
 class LGFXWidget : public Widget
 {
+private:
+    const lgfx::IFont *previousFont;
+
 protected:
     LovyanGFX *parentDisplayPtr = nullptr;
 
@@ -15,6 +18,7 @@ public:
     LGFXWidget(LovyanGFX *displayPtr, uint16_t width, uint16_t height, uint16_t xOffset, uint16_t yOffset, uint8_t padding = 0);
     ~LGFXWidget();
     virtual bool refresh(bool force = false) = 0;
+    void restorePreviousFont(void);
 };
 
 #endif // SDR_REMOTE_CONTROL_LGFX_WIDGET_H
