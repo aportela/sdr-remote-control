40802f55684393f1a8ea705d400356913b171b07
alex <766f6964+github@gmail.com>
2024-12-10 17:37:36 +0100

    added primitives (with padding + offsets included)

--- src/display/widgets/LGFXWidget.cpp
+++ src/display/widgets/LGFXWidget.cpp
@@ -20,3 +20,148 @@ void LGFXWidget::restorePreviousFont(void)
 {
     this->parentDisplayPtr->setFont(this->previousFont);
 }
+
+bool LGFXWidget::setCursor(uint16_t x, uint16_t y)
+{
+    if (x <= (this->width - this->padding) && y <= (this->height - this->padding))
+    {
+        this->parentDisplayPtr->setCursor(this->xOffsetWithPadding + x, this->yOffsetWithPadding + y);
+        return (true);
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFXWidget::printAtCursor(uint16_t x, uint16_t y, const char *str)
+{
+    if (this->setCursor(x, y))
+    {
+        this->parentDisplayPtr->print(str);
+        return (true);
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFXWidget::drawRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color)
+{
+    if (x <= (this->width - this->padding) && y <= (this->height - this->padding))
+    {
+#ifdef LGFXWIDGET_ALLOW_OVERFLOWS
+        this->parentDisplayPtr->drawRoundRect(this->xOffsetWithPadding + x, this->yOffsetWithPadding + y, width, height, color);
+        return (true);
+#else
+#error TODO
+#endif
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFXWidget::fillRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color)
+{
+    if (x <= (this->width - this->padding) && y <= (this->height - this->padding))
+    {
+#ifdef LGFXWIDGET_ALLOW_OVERFLOWS
+        this->parentDisplayPtr->fillRoundRect(this->xOffsetWithPadding + x, this->yOffsetWithPadding + y, width, height, color);
+        return (true);
+#else
+#error TODO
+#endif
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFXWidget::drawRoundRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t radius, uint32_t color)
+{
+    if (x <= (this->width - this->padding) && y <= (this->height - this->padding))
+    {
+#ifdef LGFXWIDGET_ALLOW_OVERFLOWS
+        this->parentDisplayPtr->drawRoundRect(this->xOffsetWithPadding + x, this->yOffsetWithPadding + y, width, height, radius, color);
+        return (true);
+#else
+#error TODO
+#endif
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFXWidget::fillRoundRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t radius, uint32_t color)
+{
+    if (x <= (this->width - this->padding) && y <= (this->height - this->padding))
+    {
+#ifdef LGFXWIDGET_ALLOW_OVERFLOWS
+        this->parentDisplayPtr->fillRoundRect(this->xOffsetWithPadding + x, this->yOffsetWithPadding + y, width, height, radius, color);
+        return (true);
+#else
+#error TODO
+#endif
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFXWidget::drawFastHLine(uint16_t x, uint16_t y, uint16_t width, uint32_t color)
+{
+    if (x <= (this->width - this->padding) && y <= (this->height - this->padding))
+    {
+#ifdef LGFXWIDGET_ALLOW_OVERFLOWS
+        this->parentDisplayPtr->drawFastHLine(this->xOffsetWithPadding + x, this->yOffsetWithPadding + y, width, color);
+        return (true);
+#else
+#error TODO
+#endif
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFXWidget::drawFastVLine(uint16_t x, uint16_t y, uint16_t height, uint32_t color)
+{
+    if (x <= (this->width - this->padding) && y <= (this->height - this->padding))
+    {
+#ifdef LGFXWIDGET_ALLOW_OVERFLOWS
+        this->parentDisplayPtr->drawFastVLine(this->xOffsetWithPadding + x, this->yOffsetWithPadding + y, height, color);
+        return (true);
+#else
+#error TODO
+#endif
+    }
+    else
+    {
+        return (false);
+    }
+}
+
+bool LGFXWidget::drawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint32_t color)
+{
+    if (x0 <= (this->width - this->padding) && y0 <= (this->height - this->padding))
+    {
+#ifdef LGFXWIDGET_ALLOW_OVERFLOWS
+        this->parentDisplayPtr->drawLine(this->xOffsetWithPadding + x0, this->yOffsetWithPadding + y0, this->xOffsetWithPadding + x1, this->yOffsetWithPadding + y1, color);
+        return (true);
+#else
+#error TODO
+#endif
+    }
+    else
+    {
+        return (false);
+    }
+}
--- src/display/widgets/LGFXWidget.hpp
+++ src/display/widgets/LGFXWidget.hpp
@@ -6,6 +6,8 @@
 
 #include "../../DisplayConfiguration.hpp"
 
+#define LGFXWIDGET_ALLOW_OVERFLOWS true
+
 class LGFXWidget : public Widget
 {
 private:
@@ -19,6 +21,15 @@ public:
     ~LGFXWidget();
     virtual bool refresh(bool force = false) = 0;
     void restorePreviousFont(void);
+    bool setCursor(uint16_t x, uint16_t y);
+    bool printAtCursor(uint16_t x, uint16_t y, const char *str);
+    bool drawRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+    bool fillRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+    bool drawRoundRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t radius, uint32_t color);
+    bool fillRoundRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t radius, uint32_t color);
+    bool drawFastHLine(uint16_t x, uint16_t y, uint16_t width, uint32_t color);
+    bool drawFastVLine(uint16_t x, uint16_t y, uint16_t height, uint32_t color);
+    bool drawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint32_t color);
 };
 
 #endif // SDR_REMOTE_CONTROL_LGFX_WIDGET_H
